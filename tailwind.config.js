de06d71133647a64f2c31e7f930aca7741041c36
dennis <dennysedu50@gmail.com>
2024-07-13 12:13:34 -0300

    Editei responcividade

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'custom-lg': '700px',  // Definindo um ponto de interrupção personalizado para 700px
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
