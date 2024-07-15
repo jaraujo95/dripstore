b6b4ae805e8bdd1953ddd1a3e51a66690f1d6336
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-07-15 19:57:52 -0300

    pagina sendo refeita em react

---
+++ destaques-react/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Destaques from './destaques.jsx'
+
+ReactDOM.createRoot(document.getElementById('destaques')).render(
+  <React.StrictMode>
+    <Destaques />
+  </React.StrictMode>,
+)
