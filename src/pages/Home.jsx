31c9e4ff7556381d0bd5311cfbbfad519979919f
albertverso <caralbertverso@gmail.com>
2024-07-18 19:08:54 -0300

    init project react+vite

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import '../pages/Home.css'
+import { ArrowLeft, ArrowRight } from 'react-bootstrap-icons';
+
+function Home() {
+  return (
+    <div className='body'>
+      <header className='header'>
+        <h1>Dripstore</h1>
+        <div>
+        <ArrowRight/>
+          NAV
+        <ArrowLeft/>
+        </div>
+      </header>
+
+      <section className="ignore-padding section">
+        <div>HERO</div>
+      </section>
+
+      <section className='section'>
+        <div>COLECAO</div>
+      </section>
+
+      <section className='section'>
+        <div>PRODUTOS</div>
+      </section>
+
+      <section className='section'>
+        <div>DESTAQUES</div>
+      </section>
+
+      <footer className="footer ignore-padding">
+        <div>FOOTER</div>
+      </footer>
+    </div>
+  );
+}
+      
+export default Home
