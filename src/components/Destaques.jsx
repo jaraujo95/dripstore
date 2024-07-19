d1d83ba556127c3bcb9758f139931799bde92532
Gabriel Henrique <henriquedeveloper24@gamil.com>
2024-07-19 11:31:41 -0300

    feat: commit

--- src/components/Destaques.jsx
+++ src/components/Destaques.jsx
@@ -3,34 +3,27 @@ import Camisa from '../assets/camisa.svg'
 import Tenis from '../assets/tenis.svg'
 import Fone from '../assets/fone.svg'
 import React from 'react'
-import { createElement } from 'react'
 
 function Destaques() {
-    return createElement(
+    return(
         <div className="flex justify-evenly">
-          <div className="destaques-conteiner flex flex-col items-start justify-start w-[405px] h-72 bg-blue-200 rounded-lg p-4">
-            <div className="promo bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
-            <h3 className="text-2xl font-bold mb-2">Novo drop Supreme</h3>
-            <button type="button" className="btn bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4 hover:bg-pink-600 hover:text-white">Comprar</button>
-            <div className="produto">
-              <Camisa />
-            </div>
+          <div className="relative flex flex-col items-start justify-center w-[405px] h-72 bg-blue-200 rounded-lg p-4">
+            <div className="z-10 bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
+            <h3 className="z-10 text-2xl font-bold mb-2">Novo drop Supreme</h3>
+            <button type="button" className="btn z-10 bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4 w-[153px] h-[48px] hover:bg-pink-600 hover:text-white">Comprar</button>
+              <img src={Camisa} className='absolute right-0 bottom-0'/>
           </div>
-          <div className="destaques-conteiner flex flex-col items-start justify-center w-[405px] h-72 bg-blue-200 rounded-lg p-4">
-            <div className="promo bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
-            <h3 className="text-2xl font-bold mb-2">Coleção Adidas</h3>
-            <button type="button" className="btn bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4  hover:bg-pink-600 hover:text-white">Comprar</button>
-            <div className="produto">
-              <Tenis />
-            </div>
+          <div className="relative flex flex-col items-start justify-center w-[405px] h-72 bg-blue-200 rounded-lg p-4">
+            <div className="z-10 bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
+            <h3 className="z-10 text-2xl font-bold mb-2">Coleção Adidas</h3>
+            <button type="button" className="btn z-10 bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4 w-[153px] h-[48px] hover:bg-pink-600 hover:text-white">Comprar</button>
+              <img src={Tenis} className='absolute right-0 bottom-0'/>
           </div>
-          <div className="destaques-conteiner flex flex-col items-start justify-center w-[405px] h-72 bg-blue-200 rounded-lg p-4">
-            <div className="promo bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
-            <h3 className="text-2xl font-bold mb-2">Novo Beats Bass</h3>
-            <button type="button" className="btn bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4  hover:bg-pink-600 hover:text-white">Comprar</button>
-            <div className="produto">
-              <Fone />
-            </div>
+          <div className=" relative flex flex-col items-start justify-center w-[405px] h-72 bg-blue-200 rounded-lg p-4">
+            <div className="z-10 bg-yellow-300 px-4 py-1 rounded-full font-bold mb-2">30% OFF</div>
+            <h3 className="z-10 text-2xl font-bold mb-2">Novo Beats Bass</h3>
+            <button type="button" className="btn z-10 bg-gray-100 text-pink-500 font-bold py-2 px-4 rounded-lg mb-4 w-[153px] h-[48px] hover:bg-pink-600 hover:text-white">Comprar</button>
+              <img src={Fone} className='absolute right-0 bottom-0'/>
           </div>
         </div>
     );
