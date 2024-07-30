f0fe9cba1b0ba43025d5f02455764661eebbedab
albertverso <caralbertverso@gmail.com>
2024-07-29 21:32:19 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist",
+  base: "/dripstore/",
   plugins: [react()],
   css: {
     postcss: {
