af740787d7058bc258bedbd14d3e19f5c73a0925
albertverso <caralbertverso@gmail.com>
2024-07-31 18:21:27 -0300

    config gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Rotas from './pages/Rotas'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter basename="https://albertverso.github.io/dripstore">
+<BrowserRouter basename="/dripstore">
    <Rotas></Rotas>
 </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/dripstore/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // Path to your _redirects file
+          dest: '' // The directory to copy the file to
+        }
+      ]
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
