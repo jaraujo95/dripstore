322ff89d22516837bb4712baba0acbd8c7408342
albertverso <caralbertverso@gmail.com>
2024-07-31 18:00:36 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://albertverso.github.io/dripstore",
+  base: "/dripstore/",
   plugins: [react()],
   css: {
     postcss: {
