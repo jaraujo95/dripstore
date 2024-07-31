1bfdbd4eaf8d39b7be5cca3d8422d6919e88cf50
albertverso <caralbertverso@gmail.com>
2024-07-31 19:40:50 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/dripstore/",
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
     viteStaticCopy({
